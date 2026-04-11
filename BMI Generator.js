f90593b85f9ff85f53b20a17494ace6ba839d843
First code demo <akhtarifarhin@gmail.com>
2026-04-11 21:13:24 +0530

    add text

--- BMI Generator.js
+++ BMI Generator.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e){
-    e.preventDefault();
+    e.preventDefault();  
 
     const height =  parseInt(document.querySelector('#height').value)
     const weight =  parseInt(document.querySelector('#weight').value)
